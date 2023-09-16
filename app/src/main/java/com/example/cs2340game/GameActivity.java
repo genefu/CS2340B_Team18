ee0c199d5b1eb8709fc6c9fd9dbd879f6da40ed2
impossibleguy <kwanwoo.lee63@gmail.com>
2023-09-16 17:58:21 -0400

    Added base view for app.

--- app/src/main/java/com/example/cs2340game/GameActivity.java
+++ app/src/main/java/com/example/cs2340game/GameActivity.java
@@ -1,5 +1,13 @@
 package com.example.cs2340game;
 
-public class GameActivity {
+import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+public class GameActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.welcome_view);
+    }
 }
