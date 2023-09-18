49f5947e89c711fbaf666b2279d67e20e5309187
genesis <116662964+genefu@users.noreply.github.com>
2023-09-18 16:14:23 -0400

    Fix welcome view xml, add game view xml, add transition to configuration screen from welcome view

--- app/src/main/java/com/example/cs2340game/InitialConfigView.java
+++ app/src/main/java/com/example/cs2340game/InitialConfigView.java
@@ -1,4 +1,13 @@
 package com.example.cs2340game;
 
-public class InitialConfigView {
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class InitialConfigView extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.configuration_view);
+    }
 }
--- app/src/main/java/com/example/cs2340game/WelcomeView.java
+++ app/src/main/java/com/example/cs2340game/WelcomeView.java
@@ -1,5 +1,19 @@
 package com.example.cs2340game;
 
-public class WelcomeView {
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+public class WelcomeView extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.welcome_view);
+    }
+
+    public void configScreen(View view) {
+        startActivity(new Intent(WelcomeView.this, InitialConfigView.class));
+    }
 }
