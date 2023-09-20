1414072d7016595b047c667fd27d074d51fb7532
genefu <genesis_fu@yahoo.com>
2023-09-20 14:43:50 -0400

    Add button functionaliy to every page to connect all screens, add exit game functionality

--- app/src/main/java/com/example/cs2340game/InitialConfigView.java
+++ app/src/main/java/com/example/cs2340game/InitialConfigView.java
@@ -1,6 +1,8 @@
 package com.example.cs2340game;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,5 +11,9 @@ public class InitialConfigView extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.configuration_view);
+    }
+
+    public void gameScreen(View view) {
+        startActivity(new Intent(InitialConfigView.this, GameActivity.class));
     }
 }
