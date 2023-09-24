bb7c4887a5120bff08a247597b4b81b0892905b5
genefu <genesis_fu@yahoo.com>
2023-09-24 16:26:10 -0400

    Fix BR error and get notifyPropertyChanged to work

--- app/src/main/java/com/example/cs2340game/viewmodels/ConfigurationViewModel.java
+++ app/src/main/java/com/example/cs2340game/viewmodels/ConfigurationViewModel.java
@@ -1,18 +1,21 @@
 package com.example.cs2340game.viewmodels;
 
 import androidx.databinding.BaseObservable;
+import androidx.databinding.Bindable;
 
+import com.example.cs2340game.BR;
 import com.example.cs2340game.model.Model;
 
 //View Model for Configuration Screen
 public class ConfigurationViewModel extends BaseObservable {
+
     //Setter for playerName in Model
+    @Bindable
     public void setPlayerName(String playerName) {
         Model.setPlayerName(playerName);
-        //notifyPropertyChanged(BR.playerName); TODO FIX, updates the view using binded data
+        notifyPropertyChanged(BR.playerName); //TODO FIX, updates the view using binded data
         //TODO connect to text input
     }
-
 
     //Constructor
     public ConfigurationViewModel() {
