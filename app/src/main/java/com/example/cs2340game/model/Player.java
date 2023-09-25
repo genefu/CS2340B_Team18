779bc2471760d288deca4aeed1f9ec4fe4a3ee34
songJ1128 <jerrysong1128@gmail.com>
2023-09-24 22:02:50 -0400

    displayed player stats

--- app/src/main/java/com/example/cs2340game/model/Player.java
+++ app/src/main/java/com/example/cs2340game/model/Player.java
@@ -8,7 +8,30 @@ public class Player {
     private int speed;
 
     public Player() {
+        switch (Model.getDifficulty()) {
+            case 0: health = 100;
+                    strength = 100;
+                    break;
+            case 1: health = 50;
+                    strength = 50;
+                    break;
+            case 2: health = 25;
+                    strength = 25;
+                    break;
+            default: health = 50;
+                     strength = 50;
+                     break;
+        }
+        speed = 100;
 
+
+    }
+    public int getHealth() {
+        return health;
+    }
+
+    public int getStrength() {
+        return strength;
     }
 
 }
