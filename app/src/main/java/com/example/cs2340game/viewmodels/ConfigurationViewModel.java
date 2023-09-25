a8b9507448210041f4af5e24140082ea27bcb5c5
adocys <adocys@gmail.com>
2023-09-24 22:18:57 -0400

    Adds functionality requiring player name input

--- app/src/main/java/com/example/cs2340game/viewmodels/ConfigurationViewModel.java
+++ app/src/main/java/com/example/cs2340game/viewmodels/ConfigurationViewModel.java
@@ -9,6 +9,11 @@ import com.example.cs2340game.BR;
 import com.example.cs2340game.model.Model;
 import com.example.cs2340game.model.Player;
 
+import android.widget.Button;
+import android.widget.TextView;
+import com.example.cs2340game.R;
+import android.view.View;
+
 //View Model for Configuration Screen
 public class ConfigurationViewModel extends BaseObservable {
     //@Bindable
@@ -19,16 +24,21 @@ public class ConfigurationViewModel extends BaseObservable {
     }
 
     //Setter for playerName in Model
-    public void setPlayerName(String playerName) {
-        this.playerName = playerName;
-        Model.setPlayerName(playerName);
+    public void setPlayerName(String playerName, View view) {
+        if (playerName == null || playerName.isBlank()) {
+            ((TextView) view.findViewById(R.id.nameRequirement)).setText("Premium");
+        } else {
+            this.playerName = playerName;
+            Model.setPlayerName(playerName);
+            view.findViewById(R.id.next_button).setEnabled(true);
+        }
         //notifyPropertyChanged(BR.playerName); //TODO FIX, updates the view using binded data
         //TODO connect to text input
     }
 
     //Constructor
     public ConfigurationViewModel() {
-
+        playerName = null;
     }
 
     // Sets the difficulty based on character sprite choice
--- app/src/main/java/com/example/cs2340game/views/ConfigurationView.java
+++ app/src/main/java/com/example/cs2340game/views/ConfigurationView.java
@@ -4,9 +4,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.text.Editable;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
+import android.text.TextWatcher;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -24,6 +26,21 @@ public class ConfigurationView extends AppCompatActivity implements View.OnClick
         nameSender = (EditText) findViewById(R.id.nameTextField);
         viewModel = new ConfigurationViewModel();
 
+        nameSender.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void afterTextChanged(Editable s) {}
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int swtart, int before, int count) {
+
+            }
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                if (nameSender.getText().toString() == null || nameSender.getText().toString().isBlank())
+                    ((Button)findViewById(R.id.next_button)).setEnabled(true);
+            }
+        });
+
         ImageButton easy = (ImageButton) findViewById(R.id.easy_button);
         easy.setOnClickListener(this);
         ImageButton medium = (ImageButton) findViewById(R.id.medium_button);
@@ -40,7 +57,8 @@ public class ConfigurationView extends AppCompatActivity implements View.OnClick
 
     //Switches view to GameView
     public void toGameView(View view) {
-        viewModel.setPlayerName(nameSender.getText().toString());
+        Log.d("iwantdeath", "Going to Game View");
+        viewModel.setPlayerName(nameSender.getText().toString(), view);
         //sendIntent.putExtra("nameSend",nameSender.getText().toString()); Obsolete
         startActivity(new Intent(this, GameView.class));
     }
@@ -48,6 +66,7 @@ public class ConfigurationView extends AppCompatActivity implements View.OnClick
     //registers a click in general (difficulty or sprite) and sets corresponding value
     @Override
     public void onClick(View view) {
+        Log.d("iwantdeath", "OnClick");
         if (view.getId() == R.id.easy_button) {
             viewModel.onDifficultyClicked(0);
         } else if (view.getId() == R.id.medium_button) {
@@ -60,6 +79,9 @@ public class ConfigurationView extends AppCompatActivity implements View.OnClick
             viewModel.onSpriteClicked("sprite2");
         } else if (view.getId() == R.id.sprite3) {
             viewModel.onSpriteClicked("sprite3");
+        } else if (view.getId() == R.id.next_button) {
+            Log.d("iwantdeath", "Detected next button");
+            toGameView(view);
         }
     }
 }
