f5f03e02cbf72398d227da4e5ccdecc91b3e80d0
songJ1128 <jerrysong1128@gmail.com>
2023-10-11 15:02:00 -0400

    working on the tilemap

--- app/src/main/java/com/example/cs2340game/views/TileMap.java
+++ app/src/main/java/com/example/cs2340game/views/TileMap.java
@@ -13,6 +13,7 @@ public class TileMap {
     private Bitmap myBitmap;
     private Canvas canvas;
     private Bitmap tileSet;
+    private Tile[][] tilemap;
 
     public TileMap(View gameView, String screen) {
         int height = VIEW_SIZE; //gameView.getHeight();
@@ -30,11 +31,20 @@ public class TileMap {
             this.tileSet = BitmapFactory.decodeResource(res, R.drawable.black_tile);
             this.tileSet = Bitmap.createScaledBitmap(this.tileSet, width, height, false);
         }
+        int[][] tilemap = new int[width/225][height/225];
         myBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
         canvas = new Canvas(myBitmap);
-        canvas.drawBitmap(tileSet, 0, 0, null);
+        for (int rows = 0; rows < height; rows+=225) {
+            for(int cols = 0; cols < width; cols+=225) {
+                tilemap[rows][cols].draw(canvas);
+            }
+        }
+
         gameView.draw(canvas);
 
+    }
+    public void draw(Canvas canvas) {
+        canvas.drawBitmap(myBitmap, VIEW_SIZE, VIEW_SIZE, null);
     }
 
     public Bitmap getTileSet() {
