c324bf5b45993628df242276beb11d1079b0a09e
genesis <116662964+genefu@users.noreply.github.com>
2023-10-11 11:30:26 -0400

    Checkstyle changes

--- app/src/main/java/com/example/cs2340game/views/TileMap.java
+++ app/src/main/java/com/example/cs2340game/views/TileMap.java
@@ -9,14 +9,14 @@ import android.view.View;
 import com.example.cs2340game.R;
 
 public class TileMap {
-    private final int VIEW_SIZE = 100;
+    private static final int VIEW_SIZE = 100;
     private Bitmap myBitmap;
     private Canvas canvas;
     private Bitmap tileSet;
 
     public TileMap(View gameView, String screen) {
-        int height = VIEW_SIZE;//gameView.getHeight();
-        int width = VIEW_SIZE;//gameView.getWidth();
+        int height = VIEW_SIZE; //gameView.getHeight();
+        int width = VIEW_SIZE; //gameView.getWidth();
         Log.d("iwantdeath", width + " " + height);
         Resources res = gameView.getResources();
         //set the tile set based on which game screen it is
@@ -28,7 +28,7 @@ public class TileMap {
             this.tileSet = Bitmap.createScaledBitmap(this.tileSet, width, height, false);
         } else {
             this.tileSet = BitmapFactory.decodeResource(res, R.drawable.black_tile);
-            this.tileSet = Bitmap.createScaledBitmap(this.tileSet, width,height, false);
+            this.tileSet = Bitmap.createScaledBitmap(this.tileSet, width, height, false);
         }
         myBitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
         canvas = new Canvas(myBitmap);
