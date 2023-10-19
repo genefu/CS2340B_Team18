09ddc18a0004343359caa2a36d4ce5e3dbc4323f
adocys <adocys@gmail.com>
2023-10-19 15:07:05 -0400

    sets sound for menu

--- app/src/main/java/com/example/cs2340game/views/WelcomeView.java
+++ app/src/main/java/com/example/cs2340game/views/WelcomeView.java
@@ -1,6 +1,7 @@
 package com.example.cs2340game.views;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -12,6 +13,7 @@ import com.example.cs2340game.viewmodels.WelcomeViewModel;
 
 public class WelcomeView extends AppCompatActivity {
     private WelcomeViewModel viewModel;
+    private MediaPlayer mediaPlayer;
 
     //Displays the view
     @Override
@@ -19,7 +21,10 @@ public class WelcomeView extends AppCompatActivity {
         Log.d("iwantdeath", "start");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.welcome_view);
-
+        mediaPlayer = MediaPlayer.create(this, R.raw.spooky);
+        mediaPlayer.setVolume(200.0f, 200.0f);
+        mediaPlayer.setLooping(true);
+        mediaPlayer.start();
         viewModel = new WelcomeViewModel();
     }
     //Switches view to ConfigurationView
