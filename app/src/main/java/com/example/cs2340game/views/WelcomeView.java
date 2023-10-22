801021d2a882a21ee5730286bf86fa0a5882ffde
adocys <adocys@gmail.com>
2023-10-22 18:31:08 -0500

    Adds orientation

--- app/src/main/java/com/example/cs2340game/views/WelcomeView.java
+++ app/src/main/java/com/example/cs2340game/views/WelcomeView.java
@@ -5,8 +5,10 @@ import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-
+import android.view.WindowManager;
+import android.widget.RelativeLayout;
 import androidx.appcompat.app.AppCompatActivity;
+import android.content.res.Configuration;
 
 import com.example.cs2340game.R;
 import com.example.cs2340game.viewmodels.WelcomeViewModel;
@@ -14,21 +16,30 @@ import com.example.cs2340game.viewmodels.WelcomeViewModel;
 public class WelcomeView extends AppCompatActivity {
     private WelcomeViewModel viewModel;
     private MediaPlayer mediaPlayer;
+    int orientation;
 
-    //Displays the view
+    //Displays the viewds
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         Log.d("iwantdeath", "start");
         super.onCreate(savedInstanceState);
+        orientation = this.getResources().getConfiguration().orientation;
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.welcome_view);
+
         mediaPlayer = MediaPlayer.create(this, R.raw.spooky);
         mediaPlayer.setVolume(200.0f, 200.0f);
         mediaPlayer.setLooping(true);
         mediaPlayer.start();
+
         viewModel = new WelcomeViewModel();
     }
     //Switches view to ConfigurationView
     public void toConfigurationView(View view) {
+        mediaPlayer.release();
         startActivity(new Intent(WelcomeView.this, ConfigurationView.class));
     }
 
