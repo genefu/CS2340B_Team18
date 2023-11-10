d9f209c9a142ec8f1686782cc16422406171aad6
adocys <adocys@gmail.com>
2023-11-10 18:03:56 -0500

    adds mp3 file

--- app/src/main/java/com/example/cs2340game/views/WelcomeView.java
+++ app/src/main/java/com/example/cs2340game/views/WelcomeView.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Display;
 import android.view.View;
+import android.media.MediaPlayer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
