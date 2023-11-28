f15f1a6c300cb1b4def1a3bac5cd427f35018154
genefu <genesis_fu@yahoo.com>
2023-11-28 18:55:30 -0500

    Adds score checking unit tests

--- app/src/test/java/com/example/cs2340game/Sprint5Tests.java
+++ app/src/test/java/com/example/cs2340game/Sprint5Tests.java
@@ -1,11 +1,16 @@
 package com.example.cs2340game;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
 import com.example.cs2340game.model.Model;
 import com.example.cs2340game.model.Player;
 import com.example.cs2340game.model.Powerups.BasicPowerUp;
 import com.example.cs2340game.model.Powerups.PowerUp;
 import com.example.cs2340game.model.Powerups.ScoreUpDecorator;
 import com.example.cs2340game.model.Powerups.SpeedUpDecorator;
+import com.example.cs2340game.viewmodels.GameViewModel;
+import com.example.cs2340game.views.GameView;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -32,5 +37,26 @@ public class Sprint5Tests {
         Assert.assertEquals(1, player.getAvatar().getSpeed(), .1);
         s.applyPowerUp();
         Assert.assertEquals(1.2, player.getAvatar().getSpeed(), .1);
+    }
+
+    @Test
+    public void scoreDecreaseWithTime() {
+        Model.clearInstance();
+        Model model = Model.getInstance();
+        int score = model.getScore().getScoreValue();
+        GameViewModel gameViewModel = new GameViewModel();
+        gameViewModel.decrementScore();
+        assertFalse("yay", score == model.getScore().getScoreValue());
+    }
+
+    @Test
+    public void scoreIncreasesWithEnemyDefeat() {
+        Model.clearInstance();
+        Model model = Model.getInstance();
+        Player player = Player.getInstance();
+        GameViewModel gameViewModel = new GameViewModel();
+        player.addDefeatedEnemies(1);
+        gameViewModel.updateScore(player.getEnemiesDefeated() - gameViewModel.getEnemiesDefeated());
+        assertEquals(24, model.getScore().getScoreValue());
     }
 }
